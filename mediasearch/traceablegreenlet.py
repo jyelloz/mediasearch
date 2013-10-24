4991b9854c17c86514308cb350b6cd5c5bdb3d8a
Jordan Yelloz <jordan@yelloz.me>
2013-10-23 17:20:05 -0700

    Added a Traceable Group object, too.

--- mediasearch/traceablegreenlet.py
+++ mediasearch/traceablegreenlet.py
@@ -1,13 +1,25 @@
 from uuid import uuid4
 
-from gevent import Greenlet
+from gevent import Greenlet as _Greenlet
+from gevent.pool import Group as _Group
 
 
-class TraceableGreenlet(Greenlet):
+class Greenlet(_Greenlet):
 
     def __init__(self, run=None, *args, **kwargs):
         print self
-        super(TraceableGreenlet, self).__init__(run, *args, **kwargs)
+        super(Greenlet, self).__init__(run, *args, **kwargs)
+        self._uuid = str(uuid4())
+
+    @property
+    def id(self):
+        return self._uuid
+
+
+class Group(_Group):
+
+    def __init__(self, *args):
+        super(Group, self).__init__(*args)
         self._uuid = str(uuid4())
 
     @property
