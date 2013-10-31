962209e7e9fadc513761a361485279194c0abc6d
Jordan Yelloz <jordan@yelloz.me>
2013-10-31 16:12:49 -0700

    Removed sqlalchemy requirement for initial testcase.

--- mediasearch/testsuite/test_greenlets.py
+++ mediasearch/testsuite/test_greenlets.py
@@ -1,36 +1,54 @@
 # -*- coding: utf-8 -*-
 
+from gevent import monkey
+monkey.patch_all()
+
 import logging
-import pickle
 import unittest
 
-from datetime import datetime
 from random import random
-from uuid import uuid4
+from urlparse import urlunsplit
 
-from gevent import Greenlet, monkey, sleep
-from gevent.event import AsyncResult, Event
+from gevent import Greenlet, sleep, spawn
+from gevent.event import Event
 from gevent.pool import Group
+from gevent.queue import JoinableQueue, Empty
 
-from sqlalchemy import Column, Integer, String, LargeBinary, DateTime, Text
-from sqlalchemy.engine import create_engine
-from sqlalchemy.orm import sessionmaker, scoped_session
-from sqlalchemy.ext.declarative import declarative_base
+from mediasearch.providers import SearchResult
 
 
-monkey.patch_all()
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger(__name__)
 
 
 class Service(object):
 
-    def search(self, query):
+    def search(self, query, streaming=False):
 
-        return [
-            v for v in self.VIDEOS
-            if query in v
-        ]
+        query_lower = query.lower()
+
+        results = (
+            SearchResult(v, self.url(i), self.SOURCE)
+            for (i, v) in enumerate(self.VIDEOS)
+            if query_lower in v.lower()
+        )
+
+        return (
+            results if streaming
+            else list(results)
+        )
+
+    def url(self, id):
+
+        video_url = urlunsplit((
+            'https',
+            self.NETLOC,
+            '/video/{id}'.format(id=id),
+            None,
+            None,
+        ))
+
+        return unicode(video_url)
 
 
 class FirstService(Service):
@@ -42,6 +60,8 @@ class FirstService(Service):
         'Terminator 2',
         'Videodrome',
     ]
+    SOURCE = 'First Service'
+    NETLOC = 'firstservice.example.com'
 
 
 class SecondService(Service):
@@ -51,6 +71,8 @@ class SecondService(Service):
         'Terminator 3',
         'The Jerk',
     ]
+    SOURCE = 'Second Service'
+    NETLOC = 'secondservice.example.com'
 
 
 class ThirdService(Service):
@@ -61,153 +83,85 @@ class ThirdService(Service):
         'Police Academy 4',
         'Terminator',
     ]
-
-
-Base = declarative_base()
-
-
-class SearchResult(Base):
-
-    __tablename__ = 'search_result'
-
-    pk = Column(
-        Integer,
-        primary_key=True,
-        nullable=False,
-    )
-    search_pk = Column(
-        String(255),
-        nullable=False,
-    )
-    timestamp = Column(
-        DateTime,
-        default=datetime.utcnow,
-        nullable=False,
-    )
-    source = Column(
-        Text,
-        nullable=False,
-    )
-    data = Column(
-        LargeBinary,
-        nullable=False,
-    )
+    SOURCE = 'Third Service'
+    NETLOC = 'thirdservice.example.com'
 
 
 class GreenletTestcase(unittest.TestCase):
 
     def setUp(self):
-
-        self.engine = engine = create_engine('sqlite:///test.db.sqlite3')
-        self.session_factory = session_factory = sessionmaker(bind=engine)
-        self.Session = scoped_session(session_factory)
-
-        Base.metadata.create_all(engine)
+        pass
 
     def test_greenlet(self):
 
+        queue = JoinableQueue()
         requests_done = Event()
-        callbacks_done = Event()
 
-        def a(event, service, query, search_pk):
-
-            session = self.Session()
-
-            result = service.search(query)
-
-            delay = 0.25 + (random())
-
-            source = str(service.__class__.__name__).lower()
-            log.debug('%s: sleeping for %0.2f seconds', source, delay)
-            sleep(delay)
-
-            r = [
-                SearchResult(
-                    search_pk=search_pk,
-                    data=pickle.dumps(item),
-                    source=source,
-                ) for item in result
-            ]
-            session.add_all(r)
-            session.commit()
-
-            event.set(r[0].timestamp)
-
-            self.Session.remove()
-
-        last_consumed = [0]
-
-        def b(event, search_pk, delay=0):
-
-            log.debug('waiting for results for search_pk %r', search_pk)
-
-            sleep(delay)
-
-            if callbacks_done.is_set():
-                log.debug('all responses are consumed, skipping')
-                return
-
-            if requests_done.is_set():
-                log.debug(
-                    'requests have been completed, this is the last callback'
-                )
-                callbacks_done.set()
-
-            latest = event.get()
-
-            session = self.Session()
-
-            result = session.query(SearchResult).filter(
-                SearchResult.pk > last_consumed[0],
-                SearchResult.search_pk == search_pk,
-            )
-
-            items = list(result.all())
-            count = len(items)
-
-            last_consumed[0] = max([i.pk for i in items] + last_consumed)
-
-            log.info(
-                'got %d results for search_pk %r',
-                count,
-                search_pk,
-            )
-
-            for item in items:
-                data = pickle.loads(item.data)
-                log.info('[%s] %s: %r', str(latest), item.source, data)
-
-            self.Session.remove()
-
-        search_pk = str(uuid4())
-
-        ge = AsyncResult()
-        he = AsyncResult()
-        ie = AsyncResult()
-
-        g = Greenlet(a, ge, FirstService(), 'Terminator', search_pk)
-        h = Greenlet(a, he, SecondService(), 'Terminator', search_pk)
-        i = Greenlet(a, ie, ThirdService(), 'Terminator', search_pk)
-
-        gc = Greenlet(b, ge, search_pk, 5)
-        hc = Greenlet(b, he, search_pk, 2.5)
-        ic = Greenlet(b, ie, search_pk, 0.1)
+        g = Greenlet(self._producer, queue, FirstService(), 'Terminator')
+        h = Greenlet(self._producer, queue, SecondService(), 'Terminator')
+        i = Greenlet(self._producer, queue, ThirdService(), 'Terminator')
 
         requests = Group()
-        callbacks = Group()
 
-        [requests.add(request) for request in [g, h, i]]
-        [callbacks.add(callback) for callback in [gc, hc, ic]]
+        for request in g, h, i:
+            requests.add(request)
 
         log.debug('before spawn')
 
-        [c.start() for c in callbacks]
+        c = spawn(
+            self._consumer,
+            done=requests_done,
+            queue=queue,
+        )
         [r.start() for r in requests]
 
         log.debug('after spawn')
 
         requests.join()
         requests_done.set()
-        callbacks.join()
 
-        log.debug('everything is done')
+        log.debug('requests are done')
+
+        c.join()
+
+        log.debug('consumer is done')
+
+    @staticmethod
+    def _consumer(done, queue):
+
+        log.debug('waiting for results')
+
+        while not done.is_set():
+
+            try:
+                result = queue.get(timeout=1)
+            except Empty:
+                continue
+
+            try:
+                log.debug('received %r', result)
+            finally:
+                queue.task_done()
+
+    @staticmethod
+    def _producer(queue, service, query):
+
+        results = service.search(query, streaming=True)
+
+        for result in results:
+
+            delay = 5 * random()
+
+            log.debug(
+                '%s: sleeping for %0.2f seconds',
+                service.SOURCE,
+                delay,
+            )
+            sleep(delay)
+
+            queue.put(result)
+
+        log.debug(
+            'done producing results from %r',
+            service,
+        )
